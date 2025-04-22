ae873f23ca3db0aae36c57167ef8bb67de629843
Evan <47493765+iamEvanYT@users.noreply.github.com>
2025-04-22 23:05:03 +0800

    Improved Web Store Management (#10)
    * afterInstall, afterUninstall and overrideExtensionInstallStatus
    
    * add customSetExtensionEnabled option to installChromeWebStore()
    
    * docs

--- packages/electron-chrome-web-store/src/browser/types.ts
+++ packages/electron-chrome-web-store/src/browser/types.ts
@@ -13,6 +13,26 @@ export type BeforeInstall = (
   details: ExtensionInstallDetails,
 ) => Promise<{ action: 'allow' | 'deny' }>
 
+export type AfterInstall = (details: ExtensionInstallDetails) => Promise<void>
+
+export type AfterUninstall = (details: {
+  id: ExtensionId
+  extension?: Electron.Extension
+  manifest?: chrome.runtime.Manifest
+}) => Promise<void>
+
+export type CustomSetExtensionEnabled = (
+  state: WebStoreState,
+  extensionId: ExtensionId,
+  enabled: boolean,
+) => Promise<void>
+
+export type OverrideExtensionInstallStatus = (
+  state: WebStoreState,
+  extensionId: ExtensionId,
+  manifest?: chrome.runtime.Manifest,
+) => string | undefined
+
 export interface WebStoreState {
   session: Electron.Session
   extensionsPath: string
@@ -21,4 +41,8 @@ export interface WebStoreState {
   denylist?: Set<ExtensionId>
   minimumManifestVersion: number
   beforeInstall?: BeforeInstall
+  afterInstall?: AfterInstall
+  afterUninstall?: AfterUninstall
+  customSetExtensionEnabled?: CustomSetExtensionEnabled
+  overrideExtensionInstallStatus?: OverrideExtensionInstallStatus
 }
