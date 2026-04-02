73ebe9e4a095b386ad09a1e23fb1ff2bc4fb5094
Evan <evan@iamevan.dev>
2026-04-02 17:59:55 +0100

    feat: use isolated world discovery APIs to inject into content scripts

--- packages/electron-chrome-extensions/src/preload.ts
+++ packages/electron-chrome-extensions/src/preload.ts
@@ -2,74 +2,61 @@ import { webFrame } from 'electron'
 import { injectExtensionAPIs } from './renderer'
 
 let injectInMainWorld = false
-const worldIds: number[] = []
 
 // Only load within extension page context
 if (process.type === 'service-worker' || location.href.startsWith('chrome-extension://')) {
   injectInMainWorld = true
 }
 
-// Probe for extensions isolated world
-// Extension worlds start and end values are documented here: https://www.electronjs.org/docs/latest/api/web-frame#webframesetisolatedworldinfoworldid-info
-async function scanForExtensionsIsolatedWorlds() {
-  // This function must be self-contained - runs in isolated world
-  function probeIsolatedWorld() {
-    if (globalThis.chrome && chrome.runtime && chrome.runtime.id) {
-      return chrome.runtime.id
+// Create injector function & inject into main world if needed
+const injectInIsolatedWorld = injectExtensionAPIs(injectInMainWorld)
+
+// Scan for extensions isolated worlds and inject into them
+// Requires Electron 41+ (TBD)
+if (injectInIsolatedWorld && webFrame && 'getIsolatedWorlds' in webFrame) {
+  const injectIntoExtensionIsolatedWorld = (worldId: number) => {
+    const EXTENSION_WORLD_START = 1 << 20
+    const EXTENSION_WORLD_END = 1 << 29
+    if (worldId < EXTENSION_WORLD_START || worldId >= EXTENSION_WORLD_END) {
+      return
     }
-    return null
+    injectInIsolatedWorld(worldId)
   }
 
-  const worlds: { worldId: number; extensionId: string }[] = []
-  // let continousFailedTries = 0
-
-  const EXTENSION_WORLD_START = 1 << 20
-  const EXTENSION_WORLD_END = 1 << 29
-  for (let i = EXTENSION_WORLD_START; i < EXTENSION_WORLD_END; i++) {
-    // undefined when script fails to execute
-    const extensionId: string | null | undefined = await webFrame.executeJavaScriptInIsolatedWorld(
-      i,
-      [
-        {
-          code: `(${probeIsolatedWorld}());`,
-        },
-      ],
-    )
-
-    if (extensionId) {
-      console.log('world', i, 'extensionId', extensionId)
-      worlds.push({ worldId: i, extensionId })
-    } else {
-      // I assume this is the last world, because we're scanning in ascending order
-      // Could be wrong, if that is the case then correct this logic
-      // continousFailedTries++
-      // if (continousFailedTries > 100) {
-      //   break
-      // }
-      break
-    }
+  // @ts-ignore - APIs may not be available yet
+  console.log('isolated worlds', webFrame.getIsolatedWorlds())
+  // @ts-ignore - APIs may not be available yet
+  for (const worldId of webFrame.getIsolatedWorlds()) {
+    console.log('injecting into isolated world (initial scan)', worldId)
+    injectIntoExtensionIsolatedWorld(worldId)
   }
 
-  return worlds
+  // Inject into new isolated worlds as they are created (before the script is executed)
+  // @ts-ignore - APIs may not be available yet
+  webFrame.on('isolated-world-created', (worldId) => {
+    console.log('injecting into isolated world (new world created)', worldId)
+    injectIntoExtensionIsolatedWorld(worldId)
+  })
 }
 
-async function scanAndInjectExtensionAPIs() {
-  // Service workers does not have access to webFrame global, and does not have content scripts.
-  if (webFrame) {
-    const worlds = await scanForExtensionsIsolatedWorlds()
-    worldIds.push(...worlds.map((world) => world.worldId))
-  }
+// This is for debugging purposes only (TODO: remove)
+// @ts-expect-error globalThis is not typed
+globalThis.printIsolatedWorlds = () => {
+  // @ts-ignore - APIs may not be available yet
+  const worldIds = webFrame.getIsolatedWorlds()
+  console.log('isolated worlds', worldIds)
+  for (const worldId of worldIds) {
+    const extensionIdPromise = webFrame
+      .executeJavaScriptInIsolatedWorld(worldId, [{ code: 'chrome?.runtime?.id' }])
+      .then((result) => result)
+      .catch(() => null)
 
-  // Inject extension APIs into main world and/or isolated worlds
-  const injectInIsolatedWorld = injectExtensionAPIs(injectInMainWorld)
-  if (worldIds.length > 0 && injectInIsolatedWorld) {
-    worldIds.forEach((worldId) => {
-      injectInIsolatedWorld(worldId)
+    extensionIdPromise.then((id: string | null | undefined) => {
+      if (id) {
+        console.log('extension id in world', worldId, id)
+      } else {
+        console.log('no extension id in world', worldId)
+      }
     })
   }
 }
-
-// When page first loads, it takes a bit before the extension worlds are actually created.
-// setTimeout 1000ms is too long, but not timing out means the APIs will never be injected.
-// Weird issue.
-scanAndInjectExtensionAPIs()
